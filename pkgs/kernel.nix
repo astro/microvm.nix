21baf66bb6d43e7c44b67ffaf23a5c9eae6770b6
Astro <astro@spaceboyz.net>
2021-06-24 16:15:15 +0200

    pkgs/kernel: import .config

--- pkgs/kernel.nix
+++ pkgs/kernel.nix
@@ -5,9 +5,6 @@ let
 in {
   virtioKernel = (pkgs.linuxPackages_custom {
     inherit (pkgs.linuxPackages.kernel) version src;
-    configfile = builtins.fetchurl {
-      url = "https://mergeboard.com/files/blog/qemu-microvm/defconfig";
-      sha256 = "0ml8v19ir3vmhd948n7c0k9gw8br4d70fd02bfxv9yzwl6r1gvd9";
-    };
+    configfile = ./kernel.config;
   }).kernel;
 }

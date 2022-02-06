52fd4aaeef7f114cc2b9f83705bce081d532408f
Astro <astro@spaceboyz.net>
2022-02-06 22:23:14 +0100

    checks/iperf: restore microvm configuration

--- checks/iperf.nix
+++ checks/iperf.nix
@@ -2,12 +2,43 @@
 
 nixpkgs.lib.optionalAttrs (builtins.elem hypervisor self.lib.hypervisorsWithNetwork) {
   # Run a VM with to test MicroVM virtiofsd
-  "vm-${hypervisor}-iperf" = import (nixpkgs + "/nixos/tests/make-test-python.nix") ({ ... }: {
-    name = "vm-host-microvm-${hypervisor}-virtiofsd";
+  "vm-${hypervisor}-iperf" = import (nixpkgs + "/nixos/tests/make-test-python.nix") ({ pkgs, ... }: {
+    name = "vm-${hypervisor}-iperf";
     nodes.vm = {
       imports = [ self.nixosModules.host ];
-      microvm.vms."${system}-${hypervisor}-iperf-server".flake = self;
-      environment.systemPackages = with nixpkgs.legacyPackages.${system}; [ iperf iproute ];
+      microvm.vms."${hypervisor}-iperf-server".flake = nixpkgs.legacyPackages.${system}.runCommand "${hypervisor}-iperf-server.flake" {
+        passthru.nixosConfigurations."${hypervisor}-iperf-server" = nixpkgs.lib.nixosSystem {
+          inherit system;
+          modules = [
+            self.nixosModules.microvm
+            {
+              microvm = {
+                hypervisor = hypervisor;
+                interfaces = [ {
+                  type = "tap";
+                  id = "microvm";
+                  mac = "00:02:00:01:01:01";
+                } ];
+              };
+              networking.hostName = "${hypervisor}-microvm";
+              networking = {
+                interfaces.eth0 = {
+                  useDHCP = false;
+                  ipv4.addresses = [ {
+                    address = "10.0.0.1";
+                    prefixLength = 24;
+                  } ];
+                };
+                firewall.enable = false;
+              };
+              services.iperf3.enable = true;
+            }
+          ];
+        };
+      } "touch $out";
+      environment.systemPackages = with pkgs; [ #with nixpkgs.legacyPackages.${system}; [
+        iperf iproute
+      ];
       virtualisation = {
         # larger than the defaults
         memorySize = 2048;
@@ -18,7 +49,7 @@ nixpkgs.lib.optionalAttrs (builtins.elem hypervisor self.lib.hypervisorsWithNetw
         # writableStore = true;
         # # keep the store paths built inside the VM across reboots
         # writableStoreUseTmpfs = false;
-        qemu.options = [ "-enable-kvm" ];
+        qemu.options = [ "-cpu" "kvm64,vmx=on" ];
       };
     };
     testScript = ''

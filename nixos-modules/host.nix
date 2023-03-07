25274a08dda8dc1c78f970b5163f0d551774c00a
Philippe Schaaf <philippe.schaaf@secunet.com>
2023-03-07 08:39:18 +0100

    fix VFIO user permissions
    Signed-off-by: Philippe Schaaf <philippe.schaaf@secunet.com>

--- nixos-modules/host.nix
+++ nixos-modules/host.nix
@@ -174,6 +174,17 @@ in
             fi
             echo vfio-pci > driver_override
             echo $path > /sys/bus/pci/drivers_probe
+
+            # In order to access the vfio dev the permissions must be set
+            # for the user/group running the VMM later.
+            #
+            # Insprired by https://www.kernel.org/doc/html/next/driver-api/vfio.html#vfio-usage-example
+            #
+            # assert we could get the IOMMU group number (=: name of VFIO dev)
+            [[ -e iommu_group ]] || exit 1
+            VFIO_DEV=$(basename $(readlink iommu_group))
+            echo "Making VFIO device $VFIO_DEV accessible for user"
+            chown ${user}:${group} /dev/vfio/$VFIO_DEV
             popd
           done
         '';

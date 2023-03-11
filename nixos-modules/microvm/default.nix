ff3d384934ddc89428cb21233aa8c12d58e5d78f
Astro <astro@spaceboyz.net>
2023-03-11 21:33:28 +0100

    nixos-modules/microvm/mounts: init

--- nixos-modules/microvm/default.nix
+++ nixos-modules/microvm/default.nix
@@ -6,6 +6,7 @@ self:
     ./options.nix
     ./asserts.nix
     ./system.nix
+    ./mounts.nix
     ./graphics.nix
   ];
 
---
+++ nixos-modules/microvm/mounts.nix
@@ -0,0 +1,111 @@
+{ config, lib, ... }:
+
+let
+  inherit (config.microvm) writableStoreOverlay;
+
+  inherit (import ../../lib {
+    nixpkgs-lib = lib;
+  }) defaultFsType withDriveLetters;
+
+  hostStore = builtins.head (
+    builtins.filter ({ source, ... }:
+      source == "/nix/store"
+    ) config.microvm.shares
+  );
+
+  roStore =
+    if config.microvm.storeOnBootDisk
+    then "/nix/.ro-store"
+    else hostStore.mountPoint;
+
+in
+{
+  fileSystems = lib.mkMerge [ (
+    # built-in read-only store without overlay
+    lib.optionalAttrs (
+      config.microvm.storeOnBootDisk &&
+      writableStoreOverlay == null
+    ) {
+      "/nix/store" = {
+        device = "/dev/vda2";
+        fsType = config.microvm.bootDiskType;
+        neededForBoot = true;
+      };
+    }
+  ) (
+    # host store is mounted somewhere else,
+    # bind-mount to the proper place
+    lib.optionalAttrs (
+      !config.microvm.storeOnBootDisk &&
+      config.microvm.writableStoreOverlay == null &&
+      hostStore.mountPoint != "/nix/store"
+    ) {
+      "/nix/store" = {
+        device = hostStore.mountPoint;
+        options = [ "bind" ];
+        neededForBoot = true;
+      };
+    }
+  ) (
+    # built-in read-only store for the overlay
+    lib.optionalAttrs (
+      config.microvm.storeOnBootDisk &&
+      writableStoreOverlay != null
+    ) {
+      "/nix/.ro-store" = {
+        device = "/dev/vda2";
+        fsType = config.microvm.bootDiskType;
+        neededForBoot = true;
+      };
+    }
+  ) (
+    # mount store with writable overlay
+    lib.optionalAttrs (writableStoreOverlay != null) {
+      "/nix/store" = {
+        device = "overlay";
+        fsType = "overlay";
+        neededForBoot = true;
+        options = [
+          "lowerdir=${roStore}"
+          "upperdir=${writableStoreOverlay}/store"
+          "workdir=${writableStoreOverlay}/work"
+        ];
+        depends = [ roStore writableStoreOverlay ];
+      };
+    }
+  ) ( {
+    # a tmpfs / by default. can be overwritten.
+    "/" = lib.mkDefault {
+      device = "rootfs";
+      fsType = "tmpfs";
+      options = [ "size=50%,mode=0755" ];
+      neededForBoot = true;
+    };
+  } ) (
+    # Volumes
+    builtins.foldl' (result: { mountPoint, letter, fsType ? defaultFsType, ... }:
+      result // lib.optionalAttrs (mountPoint != null) {
+        "${mountPoint}" = {
+          inherit fsType;
+          device = "/dev/vd${letter}";
+          neededForBoot = mountPoint == config.microvm.writableStoreOverlay;
+        };
+      }) {} (withDriveLetters 1 config.microvm.volumes)
+  ) (
+    # 9p/virtiofs Shares
+    builtins.foldl' (result: { mountPoint, tag, proto, source, ... }: result // {
+      "${mountPoint}" = {
+        device = tag;
+        fsType = proto;
+        options = {
+          "virtiofs" = [ "defaults" ];
+          "9p" = [ "trans=virtio" "version=9p2000.L"  "msize=65536" ];
+        }.${proto};
+        neededForBoot = (
+          source == "/nix/store" ||
+          mountPoint == config.microvm.writableStoreOverlay
+        );
+      };
+    }) {} config.microvm.shares
+  ) ];
+}

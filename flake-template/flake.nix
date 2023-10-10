7f2afbe1c9332b8eec6fa8ba165c1ce2c2fcd96a
Astro <astro@spaceboyz.net>
2023-10-10 20:12:35 +0200

    flake-template/flake: simplify a little

--- flake-template/flake.nix
+++ flake-template/flake.nix
@@ -10,41 +10,40 @@
     in {
       defaultPackage.${system} = self.packages.${system}.my-microvm;
 
-      packages.${system}.my-microvm =
-        let
-          inherit (self.nixosConfigurations.my-microvm) config;
-          # quickly build with another hypervisor if this MicroVM is built as a package
-          hypervisor = "qemu";
-        in config.microvm.runner.${hypervisor};
+      packages.${system} = {
+        my-microvm = self.nixosConfigurations.my-microvm.config.microvm.declaredRunner;
+      };
 
-      nixosConfigurations.my-microvm = nixpkgs.lib.nixosSystem {
-        inherit system;
-        modules = [
-          microvm.nixosModules.microvm
-          {
-            networking.hostName = "my-microvm";
-            users.users.root.password = "";
-            microvm = {
-              volumes = [ {
-                mountPoint = "/var";
-                image = "var.img";
-                size = 256;
-              } ];
-              shares = [ {
-                # use "virtiofs" for MicroVMs that are started by systemd
-                proto = "9p";
-                tag = "ro-store";
-                # a host's /nix/store will be picked up so that the
-                # size of the /dev/vda can be reduced.
-                source = "/nix/store";
-                mountPoint = "/nix/.ro-store";
-              } ];
-              socket = "control.socket";
-              # relevant for delarative MicroVM management
-              hypervisor = "qemu";
-            };
-          }
-        ];
+      nixosConfigurations = {
+        my-microvm = nixpkgs.lib.nixosSystem {
+          inherit system;
+          modules = [
+            microvm.nixosModules.microvm
+            {
+              networking.hostName = "my-microvm";
+              users.users.root.password = "";
+              microvm = {
+                volumes = [ {
+                  mountPoint = "/var";
+                  image = "var.img";
+                  size = 256;
+                } ];
+                shares = [ {
+                  # use "virtiofs" for MicroVMs that are started by systemd
+                  proto = "9p";
+                  tag = "ro-store";
+                  # a host's /nix/store will be picked up so that no
+                  # squashfs/erofs will be built for it.
+                  source = "/nix/store";
+                  mountPoint = "/nix/.ro-store";
+                } ];
+
+                hypervisor = "qemu";
+                socket = "control.socket";
+              };
+            }
+          ];
+        };
       };
     };
 }

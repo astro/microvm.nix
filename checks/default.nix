de7f7ce1b6dd7559c2b1c79484da16c46ec8cf90
Astro <astro@spaceboyz.net>
2024-01-10 00:40:57 +0100

    checks/default: filter known good

--- checks/default.nix
+++ checks/default.nix
@@ -70,7 +70,8 @@ let
           } ];
           testing.enableTest = builtins.elem config.microvm.hypervisor [
             # Hypervisors that support 9p
-            "qemu" "crosvm" "kvmtool"
+            "qemu" "kvmtool"
+            # "crosvm"
           ];
         };
       }) ];
@@ -83,13 +84,19 @@ let
       # overlay volume
       id = "overlay";
       modules = [ ({ config, ... }: {
-        microvm.writableStoreOverlay = "/nix/.rw-store";
-        microvm.volumes = [ {
-          image = "nix-store-overlay.img";
+        microvm = {
+          writableStoreOverlay = "/nix/.rw-store";
+          volumes = [ {
+            image = "nix-store-overlay.img";
           label = "nix-store";
-          mountPoint = config.microvm.writableStoreOverlay;
-          size = 128;
-        } ];
+            mountPoint = config.microvm.writableStoreOverlay;
+            size = 128;
+          } ];
+          testing.enableTest = builtins.elem config.microvm.hypervisor [
+            # Known good
+            "qemu" "cloud-hypervisor" "firecracker"
+          ];
+        };
       }) ];
     } ]
     # boot.systemd
@@ -100,6 +107,7 @@ let
         boot.initrd.systemd.enable = false;
       } ];
     } {
+      # yes
       id = "systemd";
       modules = [ {
         boot.initrd.systemd.enable = true;

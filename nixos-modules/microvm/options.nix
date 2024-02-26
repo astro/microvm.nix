7f93206b22131aac8fa46d787696ea58460aecab
Edmund Wu <fangkazuto@gmail.com>
2024-02-25 20:20:16 -0500

    cloud-hypervisor: add extraArgs

--- nixos-modules/microvm/options.nix
+++ nixos-modules/microvm/options.nix
@@ -407,6 +407,12 @@ in
       '';
     };
 
+    cloud-hypervisor.extraArgs = mkOption {
+      type = with types; listOf str;
+      default = [];
+      description = "Extra arguments to pass to cloud-hypervisor.";
+    };
+
     crosvm.extraArgs = mkOption {
       type = with types; listOf str;
       default = [];

3b1dac282c4f3ddd3882c91cb5c97b00c143adb4
gws <glennws@pm.me>
2025-04-24 09:58:57 +0200

    fix: error: undefined variable 'int'

--- nixos-modules/host/options.nix
+++ nixos-modules/host/options.nix
@@ -11,7 +11,7 @@
     };
     host.startupTimeout = mkOption {
       description = "Maximum allowed startup time (secs) for the vms";
-      type = int;
+      type = types.int;
       default = 150;
     };
 
